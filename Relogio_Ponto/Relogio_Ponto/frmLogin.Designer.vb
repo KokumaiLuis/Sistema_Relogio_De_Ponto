dc21f62e16c4cb332b6b0fe8ebb229155a063aa2
KokumaiLuis <85628972+KokumaiLuis@users.noreply.github.com>
2021-09-21 15:43:35 -0300

    frmLogin UI criada

---
+++ Relogio_Ponto/Relogio_Ponto/frmLogin.Designer.vb
@@ -0,0 +1,145 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLogin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtUsuario = New System.Windows.Forms.TextBox()
+        Me.lblUsuario = New System.Windows.Forms.Label()
+        Me.txtSenha = New System.Windows.Forms.TextBox()
+        Me.lblSenha = New System.Windows.Forms.Label()
+        Me.lblLogin = New System.Windows.Forms.Label()
+        Me.btnEntrar = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtUsuario
+        '
+        Me.txtUsuario.Location = New System.Drawing.Point(71, 167)
+        Me.txtUsuario.Name = "txtUsuario"
+        Me.txtUsuario.Size = New System.Drawing.Size(281, 31)
+        Me.txtUsuario.TabIndex = 0
+        '
+        'lblUsuario
+        '
+        Me.lblUsuario.AutoSize = True
+        Me.lblUsuario.Font = New System.Drawing.Font("Comic Sans MS", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblUsuario.ForeColor = System.Drawing.Color.RoyalBlue
+        Me.lblUsuario.Location = New System.Drawing.Point(70, 139)
+        Me.lblUsuario.Name = "lblUsuario"
+        Me.lblUsuario.Size = New System.Drawing.Size(72, 24)
+        Me.lblUsuario.TabIndex = 1
+        Me.lblUsuario.Text = "Usuário"
+        '
+        'txtSenha
+        '
+        Me.txtSenha.Location = New System.Drawing.Point(71, 256)
+        Me.txtSenha.Name = "txtSenha"
+        Me.txtSenha.Size = New System.Drawing.Size(281, 31)
+        Me.txtSenha.TabIndex = 0
+        '
+        'lblSenha
+        '
+        Me.lblSenha.AutoSize = True
+        Me.lblSenha.Font = New System.Drawing.Font("Comic Sans MS", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblSenha.ForeColor = System.Drawing.Color.RoyalBlue
+        Me.lblSenha.Location = New System.Drawing.Point(70, 228)
+        Me.lblSenha.Name = "lblSenha"
+        Me.lblSenha.Size = New System.Drawing.Size(59, 24)
+        Me.lblSenha.TabIndex = 1
+        Me.lblSenha.Text = "Senha"
+        '
+        'lblLogin
+        '
+        Me.lblLogin.AutoSize = True
+        Me.lblLogin.Font = New System.Drawing.Font("Comic Sans MS", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblLogin.ForeColor = System.Drawing.Color.SteelBlue
+        Me.lblLogin.Location = New System.Drawing.Point(151, 43)
+        Me.lblLogin.Name = "lblLogin"
+        Me.lblLogin.Size = New System.Drawing.Size(120, 55)
+        Me.lblLogin.TabIndex = 2
+        Me.lblLogin.Text = "Login"
+        '
+        'btnEntrar
+        '
+        Me.btnEntrar.BackColor = System.Drawing.Color.SteelBlue
+        Me.btnEntrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnEntrar.ForeColor = System.Drawing.Color.White
+        Me.btnEntrar.Location = New System.Drawing.Point(152, 326)
+        Me.btnEntrar.Name = "btnEntrar"
+        Me.btnEntrar.Size = New System.Drawing.Size(112, 34)
+        Me.btnEntrar.TabIndex = 3
+        Me.btnEntrar.Text = "Entrar"
+        Me.btnEntrar.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Comic Sans MS", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.ForeColor = System.Drawing.Color.RoyalBlue
+        Me.Label1.Location = New System.Drawing.Point(110, 422)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(196, 24)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Não possui login ainda?"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Comic Sans MS", 9.0!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.ForeColor = System.Drawing.Color.RoyalBlue
+        Me.Label2.Location = New System.Drawing.Point(151, 447)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(113, 24)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Cadastre-se"
+        '
+        'frmLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.ClientSize = New System.Drawing.Size(428, 505)
+        Me.Controls.Add(Me.btnEntrar)
+        Me.Controls.Add(Me.lblLogin)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblSenha)
+        Me.Controls.Add(Me.lblUsuario)
+        Me.Controls.Add(Me.txtSenha)
+        Me.Controls.Add(Me.txtUsuario)
+        Me.Name = "frmLogin"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtUsuario As TextBox
+    Friend WithEvents lblUsuario As Label
+    Friend WithEvents txtSenha As TextBox
+    Friend WithEvents lblSenha As Label
+    Friend WithEvents lblLogin As Label
+    Friend WithEvents btnEntrar As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
