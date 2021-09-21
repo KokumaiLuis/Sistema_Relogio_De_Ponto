539283f6d55ffc3bf9f10a610a709eebeefe901f
KokumaiLuis <85628972+KokumaiLuis@users.noreply.github.com>
2021-09-21 18:58:59 -0300

    Conexao em Desenvolvimento

--- Relogio_Ponto/Relogio_Ponto/Camada de Conexão/Conexao.vb
+++ Relogio_Ponto/Relogio_Ponto/Camada de Conexão/Conexao.vb
@@ -1,3 +1,39 @@
-﻿Public Class Conexao
+﻿Imports System.Data.SqlClient
+
+Public Class Conexao
+
+    Private Arquivo As New Arquivo
+
+    Const ConexaoDB As String = "server=localhost\SQLEXPRESS;database=GRAFADM;Integrated Security = True"
+
+    Public Sub StrConexao()
+        Dim StringConexao As String = Arquivo.LerLinha(Application.StartupPath & "\StringCon.ini", 0)
+        Try
+            ObjCon = New SqlConnection(ConexaoDB)
+            ObjCon.Open()
+        Catch ex As Exception
+            MessageBox.Show("Falha de Conexão!", "Atenção!", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End
+        Finally
+            ObjCon.Close()
+        End Try
+    End Sub
+
+    Public Sub Open()
+        Try
+            If ObjCon.State = 1 Then ObjCon.Close()
+            ObjCon.Open()
+        Catch ex As Exception
+            MessageBox.Show("Erro ao abrir Conexão!", "Error:", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Public Sub Close()
+        Try
+            If ObjCon.State = 1 Then ObjCon.Close()
+        Catch ex As Exception
+            MessageBox.Show("Erro ao fechar Conexão!", "Error:", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
 
 End Class
--- Relogio_Ponto/Relogio_Ponto/Camada de Conexão/ObjetosConexao.vb
+++ Relogio_Ponto/Relogio_Ponto/Camada de Conexão/ObjetosConexao.vb
@@ -1,3 +1,7 @@
-﻿Module ObjetosConexao
-
+﻿Imports System.Data.SqlClient
+Module ObjetosConexao
+    Public ObjCon As New SqlConnection
+    Public Dr As SqlDataReader
+    Public Da As SqlDataAdapter
+    Public Comando As New SqlCommand
 End Module
